7e20c2c73edbe1310752a3d52664e41a8025b05c
Cazier92 <bryce.cazier@gmail.com>
2023-04-10 11:26:36 -0700

    flesh out create functions for packCtrl

--- controllers/pack.js
+++ controllers/pack.js
@@ -1,4 +1,8 @@
 import { Pack } from '../models/map.js'
+import { Ball } from '../models/ball.js'
+import { Machine } from '../models/machine.js'
+import { Medicine } from '../models/medicine.js'
+import { KeyItem } from '../models/keyItem.js'
 
 const index = async (req, res) => {
   try {
@@ -20,10 +24,72 @@ const show = async (req, res) => {
   }
 }
 
+const createBall = async (req, res) => {
+  try {
+    const ball = await Ball.create(req.body)
+    const pack = await Pack.findByIdAndUpdate(
+      req.params.id,
+      { $push: { ballPocket: ball }},
+      { new: true }
+    ).populate('ballPocket')
+    res.status(201).json(pack)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
 
+const createMedicine = async (req, res) => {
+  try {
+    const medicine = await Medicine.create(req.body)
+    const pack = await Pack.findByIdAndUpdate(
+      req.params.id,
+      { $push: { medicinePocket: medicine }},
+      { new: true }
+    ).populate('medicinePocket')
+    res.status(201).json(pack)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+const createMachine = async (req, res) => {
+  try {
+    const machine = await Machine.create(req.body)
+    const pack = await Pack.findByIdAndUpdate(
+      req.params.id,
+      { $push: { machinePocket: machine }},
+      { new: true }
+    ).populate('machinePocket')
+    res.status(201).json(pack)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+const createKeyItem = async (req, res) => {
+  try {
+    const keyItem = await KeyItem.create(req.body)
+    const pack = await Pack.findByIdAndUpdate(
+      req.params.id,
+      { $push: { keyItemPocket: keyItem }},
+      { new: true }
+    ).populate('machinePocket')
+    res.status(201).json(pack)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
 
 
 export { 
   index,
   show,
+  createBall,
+  createMedicine,
+  createMachine,
+  createKeyItem,
 }
