1bddd4820d9c2f5bf3c71ea780ec4b137a20340e
Cazier92 <bryce.cazier@gmail.com>
2023-04-12 13:18:37 -0700

    complete useMove function for battles

--- controllers/battle.js
+++ controllers/battle.js
@@ -1,7 +1,5 @@
 import { Pokemon } from '../models/pokemon.js'
-import { Profile } from '../models/profile.js'
 import { Move } from '../models/move.js'
-import axios from 'axios'
 import * as algorithms from '../data/algorithms.js'
 
 
@@ -10,7 +8,23 @@ const useMove = async (req, res) => {
     const move = await Move.findById(req.params.moveId)
     const user = await Pokemon.findById(req.params.userId)
     const target = await Pokemon.findById(req.params.targetId)
-    
+    const damage = algorithms.calcDamage(user, target, move)
+
+    if (target.currentHP - damage > 0) {
+      const updatedTarget = await Pokemon.findByIdAndUpdate(
+        req.params.targetId,
+        { currentHp: (target.currentHP - damage)},
+        { new: true }
+      )
+      res.status(201).json(updatedTarget)
+    } else {
+      const updatedTarget = await Pokemon.findByIdAndUpdate(
+        req.params.targetId,
+        { currentHp: 0},
+        { new: true }
+      )
+      res.status(201).json(updatedTarget)
+    }
   } catch (error) {
     console.log(error)
     res.status(500).json(error)
